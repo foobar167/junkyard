466cffa0806c5fa1981afa4759640c9fa785bb21
foobar167 <foobar167@gmail.com>
2017-04-13 12:36:44 +0300

    Create count_cameras.py
    Count number of available cameras via OpenCV.

---
+++ count_cameras.py
@@ -0,0 +1,13 @@
+import cv2
+
+# Get the number of cameras available
+def count_cameras():
+    max_tested = 100
+    for i in range(max_tested):
+        temp_camera = cv2.VideoCapture(i)
+        if temp_camera.isOpened():
+            temp_camera.release()
+            continue
+        return i
+
+print(count_cameras())
