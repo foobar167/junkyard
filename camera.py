41ab02b1001827357ae5f7a098452bfb78bd3c54
foobar167 <foobar167@gmail.com>
2017-04-17 12:00:24 +0300

    Simple window with video stream from camera.

---
+++ camera.py
@@ -0,0 +1,13 @@
+# This script displays a webcam feed (assuming that a webcam is plugged in).
+
+import cv2  # import OpenCV 3 module
+
+camera = cv2.VideoCapture(0)  # get default camera
+while True:
+    ok, frame = camera.read()  # read frame
+    if ok:  # frame is read correctly
+        cv2.imshow('My camera', frame)  # show frame
+    if cv2.waitKey(1) == 27:  # <Escape> key
+        break  # exit from cycle
+camera.release()  # release web camera
+cv2.destroyAllWindows()
