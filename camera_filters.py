3f5ebb4184078c918f4bfbef69eca2678f733cb7
foobar167 <foobar167@gmail.com>
2017-04-29 13:37:49 +0300

    Added adaptive Gaussian thresholding.

--- camera_filters.py
+++ camera_filters.py
@@ -12,12 +12,18 @@ while True:
         if mode == 3:
             frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)  # convert to grayscale
             frame = cv2.Canny(frame, 100, 200)  # Canny edge detection
+        if mode == 4:
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)  # convert to grayscale
+            #frame = cv2.medianBlur(frame, 5)  # apply blur filter
+            frame = cv2.adaptiveThreshold(frame, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, \
+                                          cv2.THRESH_BINARY, 11, 2)  # adaptive Gaussian thresholding
         cv2.imshow('My camera', frame)  # show frame
     key = cv2.waitKey(1) & 0xff  # read keystroke
     if key == 27: break  # <Escape> key pressed, exit from cycle
     if key == ord('1'): mode = 1  # show unchanged frame
     if key == ord('2'): mode = 2  # apply Canny edge detection
     if key == ord('3'): mode = 3  # apply Canny to gray frame
+    if key == ord('4'): mode = 4  # adaptive Gaussian thresholding
 
 camera.release()  # release web camera
 cv2.destroyAllWindows()
