2a4c13d46cb7168482985af4ab7eeaf251042a09
foobar167 <foobar167@gmail.com>
2017-04-29 12:15:37 +0300

    Apply Canny edge detection to grayscale. No big difference with colored Canny edge detection.

--- camera_filters.py
+++ camera_filters.py
@@ -9,11 +9,15 @@ while True:
     if ok:  # frame is read correctly
         if mode == 2:
             frame = cv2.Canny(frame, 100, 200)  # Canny edge detection
+        if mode == 3:
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)  # convert to grayscale
+            frame = cv2.Canny(frame, 100, 200)  # Canny edge detection
         cv2.imshow('My camera', frame)  # show frame
     key = cv2.waitKey(1) & 0xff  # read keystroke
     if key == 27: break  # <Escape> key pressed, exit from cycle
     if key == ord('1'): mode = 1  # show unchanged frame
     if key == ord('2'): mode = 2  # apply Canny edge detection
+    if key == ord('3'): mode = 3  # apply Canny to gray frame
 
 camera.release()  # release web camera
 cv2.destroyAllWindows()
