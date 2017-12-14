802d87ac1f556bb124dae11711de889304a9ecdc
foobar167 <foobar167@gmail.com>
2017-12-14 15:25:57 +0300

    Using lambda keyword to create a dynamic menu

--- dynamic_meny.py
+++ dynamic_meny.py
@@ -1,15 +1,27 @@
-# Using lambda keyword to create a dynamic menu.
+# Using lambda keyword and refresh function to create a dynamic menu.
 import tkinter as tk
 
-def f(x):
-    print(x)
+def show(x):
+    """ Show menu items """
+    var.set(x)
+
+def refresh(l):
+    """ Refresh menu contents """
+    var.set('')
+    menu.delete(0, 'end')
+    for i in l:
+        menu.add_command(label=i, command=lambda x=i: show(x))
 
 root = tk.Tk()
 menubar = tk.Menu(root)
 root.configure(menu=menubar)
 menu = tk.Menu(menubar, tearoff=False)
+menubar.add_cascade(label='Choice', menu=menu)
+
+var = tk.StringVar()
 l = ['one', 'two', 'three']
-for i in l:
-    menu.add_command(label=i, command=lambda x=i: f(x))
-menubar.add_cascade(label='File', menu=menu)
+refresh(l)
+l = ['four', 'five', 'six', 'seven']
+tk.Button(root, text='Refresh', command=lambda: refresh(l)).pack()
+tk.Label(root, textvariable=var).pack()
 root.mainloop()
---
+++ optionmenu_example.py
@@ -0,0 +1,23 @@
+import tkinter as tk
+
+root = tk.Tk()
+choices = ('network one', 'network two', 'network three')
+var = tk.StringVar(root)
+
+def refresh():
+    # Reset var and delete all old options
+    var.set('')
+    network_select['menu'].delete(0, 'end')
+
+    # Insert list of new options (tk._setit hooks them up to var)
+    new_choices = ('one', 'two', 'three')
+    for choice in new_choices:
+        network_select['menu'].add_command(label=choice, command=tk._setit(var, choice))
+
+network_select = tk.OptionMenu(root, var, *choices)
+network_select.grid()
+
+# I made this quick refresh button to demonstrate
+tk.Button(root, text='Refresh', command=refresh).grid()
+
+root.mainloop()
