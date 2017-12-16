a387205a47b7aa36636ab97de5fcde9ac6c42802
foobar167 <foobar167@gmail.com>
2017-12-16 23:25:14 +0300

    Tkinter Frames example

---
+++ tkinter_frames.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+from tkinter import ttk
+
+root = tk.Tk()
+root.geometry('800x600+0+0')
+root.rowconfigure(0, weight=1)
+root.columnconfigure(0, weight=3)
+root.columnconfigure(1, weight=1)
+f1 = tk.Frame(background='blue')
+f1.grid(row=0, column=0, sticky='nswe')
+f2 = tk.Frame(background='green')
+f2.grid(row=0, column=1, sticky='nswe')
+root.mainloop()
