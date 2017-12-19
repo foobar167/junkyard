8c97f0ffca75b98d115ad1e705e2c25da7f58f27
foobar167 <foobar167@gmail.com>
2017-12-19 15:12:16 +0300

    Updated dynamic menu. Made it even more dynamic.

--- dynamic_menu.py
+++ dynamic_menu.py
@@ -2,26 +2,40 @@
 import tkinter as tk
 
 def show(x):
-    """ Show menu items """
-    var.set(x)
+    """ Show your choice """
+    global label
+    new_label = 'Choice is: ' + x
+    menubar.entryconfigure(label, label=new_label)  # change menu text
+    label = new_label  # update menu label to find it next time
+    choice.set(x)
 
-def refresh(l):
+def refresh():
     """ Refresh menu contents """
-    var.set('')
-    menu.delete(0, 'end')
+    global label, l
+    if l[0] == 'one':
+        l = ['four', 'five', 'six', 'seven']
+    else:
+        l = ['one', 'two', 'three']
+    choice.set('')
+    menu.delete(0, 'end')  # delete previous contents of the menu
+    menubar.entryconfigure(label, label=const_str)  # change menu text
+    label = const_str  # update menu label to find it next time
     for i in l:
         menu.add_command(label=i, command=lambda x=i: show(x))
 
 root = tk.Tk()
+# Set some variables
+choice = tk.StringVar()
+const_str = 'Choice'
+label = const_str
+l = ['dummy']
+# Create some widgets
 menubar = tk.Menu(root)
 root.configure(menu=menubar)
 menu = tk.Menu(menubar, tearoff=False)
-menubar.add_cascade(label='Choice', menu=menu)
-
-var = tk.StringVar()
-l = ['one', 'two', 'three']
-refresh(l)
-l = ['four', 'five', 'six', 'seven']
-tk.Button(root, text='Refresh', command=lambda: refresh(l)).pack()
-tk.Label(root, textvariable=var).pack()
+menubar.add_cascade(label=label, menu=menu)
+b = tk.Button(root, text='Refresh menu', command=refresh)
+b.pack()
+b.invoke()
+tk.Label(root, textvariable=choice).pack()
 root.mainloop()
