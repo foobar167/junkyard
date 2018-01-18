502cff8b22c06681888fdfb9c3a2eee6e22d0822
foobar167 <foobar167@gmail.com>
2018-01-18 15:17:31 +0300

    Keyboard events handler.

---
+++ keyboard_events.py
@@ -0,0 +1,22 @@
+#It is possible to use Python + Tkinter to generate keyboard events.
+#Tkinter is a part of the Python.
+#Note: this is a console application :-)
+import tkinter as tk
+
+def keystroke(event):
+    print(event.keycode)
+
+def keyboardevent(str):
+    # Code that simulated 'key' being pressed on keyboard
+    root.after(10, lambda: root.event_generate('<Key-{}>'.format(str)))
+
+root = tk.Tk()
+root.withdraw()  # remove the window from the screen (without destroying it)
+root.bind('<Key>', keystroke)
+keyboardevent('a') # lower case 'a'
+keyboardevent('B') # upper case 'B'
+keyboardevent('Right') # right arrow key
+root.after(20, root.destroy)
+root.mainloop()
+
+print('Hello World!')
--- tkinter_independend_keycodes.py
+++ tkinter_independend_keycodes.py
@@ -1,6 +1,35 @@
+# 1. Generate keystrokes using Python + Tkinter.
+# 2. Save needed keycodes in the dictionary.
+# 3. Use dictionary with keycodes to handle keystrokes independently from
+#    operating system, language or (maybe?) keyboard model.
+# This is not an elegant solution, so I think it is better to "hardcode"
+# keycodes depending on the operating system.
 import tkinter as tk
-from tkinter import ttk
+
+def keystroke(event):
+    dict[event.keycode] = event.keysym  # save keycodes into the dictionary
+
+def keyboardevent(str):
+    # Code that simulated 'key' being pressed on keyboard
+    temp.after(10, lambda: temp.event_generate('<Key-{}>'.format(str)))
+
+temp = tk.Tk()
+temp.withdraw()  # remove the window from the screen (without destroying it)
+temp.bind('<Key>', keystroke)
+dict = {}  # dictionary of the needed keycodes
+keyboardevent('w')  # generate needed keyboard events
+keyboardevent('s')
+keyboardevent('a')
+keyboardevent('d')
+temp.after(20, temp.destroy)  # this is not needed anymore
+temp.mainloop()
+
+# Start your code here
+def keys_handler(event):
+    if event.keycode in dict:
+        print(dict[event.keycode])
 
 root = tk.Tk()
-
+root.focus_force()
+root.bind('<Key>', keys_handler)
 root.mainloop()
