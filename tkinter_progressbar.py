3abdb58fadf81e03daa43ae6fcea3d9ed4df47b5
foobar167 <foobar167@gmail.com>
2018-03-29 22:17:04 +0300

    Modal dialog window with Progressbar

--- tkinter_progressbar.py
+++ tkinter_progressbar.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Modal dialog with Progressbar window for the bigger project
+# Modal dialog window with Progressbar for the bigger project
 import time
 import tkinter as tk
 from tkinter import ttk
@@ -14,9 +14,8 @@ class MainGUI(ttk.Frame):
         self.master.geometry('300x200')
         self.lst = [
             'Bushes01.png',  'Bushes02.png', 'Bushes03.png', 'Bushes04.png', 'Bushes05.png',
-            'Forest01.png',  'Forest02.png', 'Forest03.png', 'Forest04.png',
-            'Road01.png',    'Road02.png',   'Road03.png',
-            'Village01.png', 'Village02.png']
+            'Forest01.png',  'Forest02.png', 'Forest03.png', 'Forest04.png', 'Road01.png',
+            'Road02.png',    'Road03.png',   'Lake01.png',   'Lake02.png',   'Field01.png']
         b = ttk.Button(self.master, text='Start', command=self.start_progress)
         b.pack()
         b.focus_set()
@@ -28,7 +27,7 @@ class MainGUI(ttk.Frame):
 
 class ProgressWindow(simpledialog.Dialog):
     def __init__(self, parent, name, lst):
-        ''' Init progress window for descriptors calculation '''
+        ''' Init progress window '''
         tk.Toplevel.__init__(self, master=parent)
         self.name = name
         self.lst = lst
@@ -39,15 +38,15 @@ class ProgressWindow(simpledialog.Dialog):
 
     def create_window(self):
         ''' Create progress window '''
-        self.focus_set()  # set focus on the settings window
-        self.grab_set()  # make a modal window, so all events go to settings window
+        self.focus_set()  # set focus on the ProgressWindow
+        self.grab_set()  # make a modal window, so all events go to the ProgressWindow
         self.transient(self.master)  # show only one window in the task bar
         #
-        self.title(u'Calculating descriptors for {}'.format(self.name))
+        self.title(u'Calculate something for {}'.format(self.name))
         self.resizable(False, False)  # window is not resizable
         # self.close gets fired when the window is destroyed
         self.protocol(u'WM_DELETE_WINDOW', self.close)
-        # Set proper settings position over the parent window
+        # Set proper position over the parent window
         dx = (self.master.master.winfo_width() >> 1) - (self.length >> 1)
         dy = (self.master.master.winfo_height() >> 1) - 50
         self.geometry(u'+{x}+{y}'.format(x = self.master.winfo_rootx() + dx,
@@ -60,34 +59,41 @@ class ProgressWindow(simpledialog.Dialog):
         self.var2 = tk.StringVar()
         self.num = tk.IntVar()
         self.maximum = len(self.lst)
+        self.tmp_str = ' / ' + str(self.maximum)
+        #
         # pady=(0,5) means margin 5 pixels to bottom and 0 to top
-        ttk.Label(self, textvariable=self.var1).pack(anchor='w', pady=(0, 5))
+        ttk.Label(self, textvariable=self.var1).pack(anchor='w', padx=2)
         self.progress = ttk.Progressbar(self, maximum=self.maximum, orient='horizontal',
                                         length=self.length, variable=self.num, mode='determinate')
-        self.progress.pack()
-        ttk.Label(self, textvariable=self.var2).pack(anchor='w')
+        self.progress.pack(padx=2, pady=2)
+        ttk.Label(self, textvariable=self.var2).pack(side='left', padx=2)
+        ttk.Button(self, text='Cancel', command=self.close).pack(anchor='e', padx=1, pady=(0, 1))
+        #
         self.next()
 
     def next(self):
-        ''' Start descriptors calculation '''
-        s = ' / ' + str(self.maximum)
+        ''' Take next file from the list and do something with it '''
         n = self.num.get()
-        print(n+1, self.lst[n])
+        self.do_something_with_file(n+1, self.lst[n])  # some useful operation
         self.var1.set('File name: ' + self.lst[n])
         n += 1
-        self.var2.set(str(n) + s)
+        self.var2.set(str(n) + self.tmp_str)
         self.num.set(n)
         if n < self.maximum:
             self.after(500, self.next)  # call itself after some time
         else:
             self.close()  # close window
 
+    def do_something_with_file(self, number, name):
+        ''' Some useful operation is here '''
+        print(number, name)
+
     def close(self, event=None):
         ''' Close progress window '''
         if self.progress['value'] == self.maximum:
-            print('Descriptors calculated successfully')
+            print('Ok: process finished successfully')
         else:
-            print('Descriptors calculation cancelled')
+            print('Cancel: process is cancelled')
         self.master.focus_set()  # put focus back to the parent window
         self.destroy()  # destroy progress window
 
