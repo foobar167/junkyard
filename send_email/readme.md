998110654cf4706a16e0578721ba3ee5835aa924
FooBar167 <foobar167@gmail.com>
2019-09-16 23:43:46 +0300

    Software update

--- send_email/readme.md
+++ send_email/readme.md
@@ -4,12 +4,12 @@
 
 #### Description
 
-[quickstart.py](quickstart/quickstart.py) is a simple Python command-line
-application that makes requests to the Gmail API
-
 Send email using `SMTP_SSL()` in file
 [send_email_using_smtp_ssl.py](send_email_using_smtp_ssl.py).
 
 Send email using `.starttls()` in file
 [send_email_using_starttls.py](send_email_using_starttls.py).
+
+[quickstart.py](quickstart/quickstart.py) is a simple Python command-line
+application that makes requests to the Gmail API
 
--- send_email/send_email_using_starttls.py
+++ send_email/send_email_using_starttls.py
@@ -1,19 +1,71 @@
 # -*- coding: utf-8 -*-
 # Create an unsecured SMTP connection and encrypt it using .starttls()
+import os
 import ssl
 import smtplib
 import getpass
+
+from email import encoders
+from email.mime.base import MIMEBase
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 
 smtp_server = 'smtp.gmail.com'
 port = 587  # starttls port
 sender_email = 'foo.bar166@gmail.com'
 receiver_email = 'foo.bar167@gmail.com'
+password = getpass.getpass(prompt='Enter password for {}: '.format(sender_email), stream=None)
 
-message = """\
-Subject: Hi there
+# Create the plain-text and HTML version of your message
+subject = 'Multipart test email with attachment - 2'
 
-This message is sent from Python.
+text = """\
+Hi, :-)
+How are you?
+Real Python has many great tutorials:
+www.realpython.com"""
+
+html = """\
+<html>
+  <body>
+    <p>Hi,<br>
+       How are you?<br>
+       <a href="http://www.realpython.com">Real Python</a> 
+       has many great tutorials.
+    </p>
+  </body>
+</html>
 """
+
+# Add HTML/plain-text parts to MIMEMultipart message
+# The email client will try to render the last part first
+message = MIMEMultipart('alternative')
+message['Subject'] = subject
+message['From'] = sender_email
+message['To'] = receiver_email
+message["Bcc"] = receiver_email  # recommended for mass emails
+message.attach(MIMEText(text, 'plain'))
+message.attach(MIMEText(html, 'html'))
+
+filepath = '../data/doge2.jpg'
+filename = os.path.basename(filepath)
+
+# Open file in binary mode
+with open(filepath, 'rb') as attachment:
+    # Add file as application/octet-stream
+    # Email client can usually download this automatically as attachment
+    part = MIMEBase('application', 'octet-stream')
+    part.set_payload(attachment.read())
+
+# Encode file in ASCII characters to send by email
+encoders.encode_base64(part)
+
+# Add header as key/value pair to attachment part
+part.add_header('Content-Disposition', f'attachment; filename= {filename}',)
+
+# Add attachment to message and convert message to string
+message.attach(part)
+text = message.as_string()
 
 # Create a secure SSL context
 context = ssl.create_default_context()
@@ -22,12 +74,9 @@ context = ssl.create_default_context()
 server = None
 try:
     server = smtplib.SMTP(smtp_server, port)
-    server.ehlo()  # can be omitted
     server.starttls(context=context)  # encrypt the connection
-    server.ehlo()  # can be omitted
-    password = getpass.getpass(prompt='Enter password for {}: '.format(sender_email), stream=None)
     server.login(sender_email, password)
-    server.sendmail(sender_email, receiver_email, message)
+    server.sendmail(sender_email, receiver_email, text)
 except smtplib.SMTPAuthenticationError as err:
     print(err.smtp_error.decode())
 finally:
