8f6fa8e2b26d5096f4fc4804bfd2c46329d6ad9e
Dzmitry Paulenka <dmitri.pavlenko@gmail.com>
2019-11-14 15:15:55 +0300

    Software update

--- simple_scripts/download_images.py
+++ simple_scripts/download_images.py
@@ -2,19 +2,27 @@
 # This is a command-line python program to search keywords / key-phrases on Google Images
 # and optionally download images to your computer.
 #
-# If you would want more than 100 images per keyword, then you would need to install
-# 'Selenium' library along with 'chromedriver'.
+# For more than 100 images per keyword install 'Selenium' library along with 'chromedriver'.
+# ChromeDriver - WebDriver for Chrome: https://sites.google.com/a/chromium.org/chromedriver/downloads
 #
 # Requirements:
 #   pip install google_images_download
+import os
+
 from google_images_download import google_images_download   # importing the library
 
 # Prepare list of arguments
-download_dir = '../temp/downloads'  # download directory will be created automatically
-arguments = {'keywords': 'balloon, tree, computer',
-             'limit': 20,
-             'print_urls': True,
-             'output_directory': f'{download_dir}'}
+output_dir = '../temp/downloads'
+os.makedirs(output_dir, exist_ok=True)  # create directory
+keywords = ['balloon tree',
+            'computer cake',]
+arguments = {
+    'keywords': ', '.join(keywords),  # use ',' between words for several requests
+    'limit': 120,  # download 120 images for EACH keyword, separated by ','
+    'print_urls': True,  # print URLs
+    'output_directory': output_dir,  # download dir will be created automatically
+    'chromedriver': '../temp/chromedriver.exe',  # path to chromedriver.exe file
+}
 
 response = google_images_download.googleimagesdownload()   # class instantiation
 output = response.download(arguments)   # download with the arguments
